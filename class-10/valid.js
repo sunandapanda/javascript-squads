1962fc3314d0c8206e55e119368e0a4584f3c779
dashb <sunandapanda.online@gmail.com>
2021-05-03 12:46:19 -0500

    added first name and last name validations

--- class-10/valid.js
+++ class-10/valid.js
@@ -1,5 +1,37 @@
 function validate(){
     
     var firstName = document.getElementById("fname").value;
-    console.log(firstName)
+    var regexPatt = /[^a-z]/i
+    var validFirstName = regexPatt.test(firstName);
+
+    console.log(firstName);
+    if(!firstName){
+      alert("Please enter first name")
+      return false;
+    }
+    
+    if(validFirstName){
+      alert("Enter a valid first name")
+      return false;
+    }
+    
+    var lastName = document.getElementById("lname").value;
+    var regexPatt = /[^a-z]/i
+    var validFirstName = regexPatt.test(lastName);
+
+    lastName = regexPatt.test(lastName);
+
+    console.log(lastName);
+    if(!lastName){
+      alert("Please enter last name")
+      return false;
+    }
+    
+    if(lastName){
+      alert("Enter a valid last name")
+      return false;
+    }
+    alert("successfully registered")
+    document.form1.submit()
+    
 }
